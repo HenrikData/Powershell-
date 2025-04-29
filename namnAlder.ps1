1d94e28d6e7356a32ce3ddd50ddf4b449d827ea8
HenrikData <henrik.karlsson2.student@ksgyf.se>
2025-04-29 09:22:12 +0200

    test

---
+++ namnAlder.ps1
@@ -0,0 +1,11 @@
+# Skapa två variabler och lägg ihop dem för att sedan skriva ut
+
+# Variabel för namn
+$namn = "Alice"
+
+
+# Variabel för ålder
+$alder = 25
+
+# Print variabler
+Write-Host "Hej $namn, du är $alder år gammal"
